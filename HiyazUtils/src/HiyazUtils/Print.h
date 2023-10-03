3163f75385a225c8b46fc5f761ed0b17de8f434c
Trevor Rose <59979141+Hiyazcool@users.noreply.github.com>
2023-10-03 18:58:36 -0500

    Update Print.h
    Updated IPrintable Implementation

--- HiyazUtils/src/HiyazUtils/Print.h
+++ HiyazUtils/src/HiyazUtils/Print.h
@@ -4,9 +4,10 @@
 #include "Format.h"
 namespace HiyazUtils {
 namespace Print {
-	class IPrintable {
-		public:
-			virtual char* PrintInfo() = 0;
+	class IPrintable {  // Seperate Into Seperate File So Format and Prin Can use it without conflicting with eachother
+			virtual const char* const PrintInfo() = 0;
+		public: 
+			operator const char*() const { return PrintInfo; }
 	};
 	template<typename Args&...args>
 	inline const static void const Print(Args& ..._args) {
@@ -15,17 +16,15 @@ namespace Print {
 	inline const static void const Print(const char* _pointer);
 	inline const static void const Print(const std::string _pointer);
 	inline const static void const Print(const string* _pointer);
-	inline const static void const Print(const IPrintable _pointer);
 
 	template<typename T, typename Args&...args>
 	inline const static void const PrintL(bool _isSeperateLines = True, T _string, Args& ..._args) {
 		if (_isSeperateLines) 
-			Print(FormatStringList(_string, _args));
+			Print(FormatIntoStringList(_string, _args));
 		else
 			PrintL(FormatString(_string, _args));
 	}
 	inline const static void const PrintL(const char* _pointer);
 	inline const static void const PrintL(const std::string _pointer);
 	inline const static void const PrintL(const string* _pointer);
-	inline const static void const PrintL(const IPrintable _pointer);
 }}
