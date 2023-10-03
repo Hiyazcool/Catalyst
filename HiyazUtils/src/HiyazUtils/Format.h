5a86b5ce918148753a67836ba78e3b59202c5488
Trevor Rose <59979141+Hiyazcool@users.noreply.github.com>
2023-10-03 18:25:19 -0500

    Update Format.h
    Laid out major Functions and String constants
    Created namespace Format

--- HiyazUtils/src/HiyazUtils/Format.h
+++ HiyazUtils/src/HiyazUtils/Format.h
@@ -1,11 +1,24 @@
 #pragma once
 #include "String.h"
 using namespace HiyazUtils;
-namespace HiyazUtils {
-	class Format {
-		/*
-			
-		*/
-	public:
-	};
-}
+namespace HiyazUtils { 
+namespace Format {
+	namespace StringConstants {
+		constexpr char* Colon = ": ";
+		constexpr char* Comma = ", ";
+		constexpr char* NewLine = "\n";
+	}
+	template<typename Args&... args>
+	static string& FormatIntoString(Args& ..._args);
+	template<typename Args&... args>
+	static void FormatString(string* _destination, Args& ..._args);
+	static void FillCharArray(char* _destination, char* _source);
+	static void FillCharArray(char* _destination, char*&& _source); // rValue
+	static void FillCharArray(char* _destination, string* _source);
+	template<typename Args&... args>
+	static size_t CalculatestringLength(char* _string, Args& ..._args);
+	template<typename Args&... args>
+	static size_t CalculatestringLength(string* _string, Args& ..._args);
+	static size_t CalculatestringLength(char* _string);
+	static size_t CalculatestringLength(string* _string);
+}}
